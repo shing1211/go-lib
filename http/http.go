13aa808a249e5cdad15f694297cbd0f5ddcd62cc
Terence Chan <terence.chan@infocast.com.hk>
2022-08-05 16:01:08 +0800

    add http post and put wrapper func

--- http/http.go
+++ http/http.go
@@ -1,27 +1,65 @@
 package http
 
 import (
+	"bytes"
 	"io/ioutil"
 	"net/http"
 	"time"
 )
 
-func HttpGetRequest(url string) ([]byte, error) {
-	client := http.Client{
-		Timeout: time.Second * 5, // Timeout after 5 seconds
-	}
+func HttpGetRequest(url string, timeout time.Duration) (int, []byte, error) {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		return nil, err
+		return 500, nil, err
 	}
-	res, getErr := client.Do(req)
-	if getErr != nil {
-		return nil, err
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	client := &http.Client{Timeout: time.Second * timeout}
+	res, err := client.Do(req)
+	if err != nil {
+		return 500, nil, err
 	}
 	defer res.Body.Close()
-	retbody, readErr := ioutil.ReadAll(res.Body)
-	if readErr != nil {
-		return nil, err
+	retbody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return 500, nil, err
 	}
-	return retbody, nil
+	return res.StatusCode, retbody, nil
+}
+
+func HttpPostRequest(url string, timeout time.Duration, jsonReq []byte) (int, []byte, error) {
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonReq))
+	if err != nil {
+		return 500, nil, err
+	}
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	client := &http.Client{Timeout: time.Second * timeout}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 500, nil, err
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 500, nil, err
+	}
+	return resp.StatusCode, bodyBytes, nil
+}
+
+func HttpPutRequest(url string, timeout time.Duration, jsonReq []byte) (int, []byte, error) {
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(jsonReq))
+	if err != nil {
+		return 500, nil, err
+	}
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	client := &http.Client{Timeout: time.Second * timeout}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 500, nil, err
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 500, nil, err
+	}
+	return resp.StatusCode, bodyBytes, err
 }
