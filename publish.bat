0d7d59d52ded53c3f11ed45cedf8fed55cc7fc16
Terence Chan <shing1211@gmail.com>
2024-03-19 01:09:35 +0800

    uu

--- publish.bat
+++ publish.bat
@@ -3,5 +3,4 @@ del .\go.mod
 go mod init github.com/shing1211/go-lib
 go mod tidy
 set GOPROXY=proxy.golang.org
-go list -m github.com/shing1211/go-lib
-go list -m github.com/shing1211/go-lib@latest
+go list -m -u github.com/shing1211/go-lib@latest
