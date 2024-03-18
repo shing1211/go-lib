7547e4b85ccb49f0f939f9c2ed1a81ec2b6b0f44
Terence Chan <38172860+shing1211@users.noreply.github.com>
2024-03-18 18:58:17 +0800

    update string function

--- string/string.go
+++ string/string.go
@@ -1,10 +1,15 @@
 package string
 
-import "strconv"
+import (
+	"strconv"
+
+	log "github.com/sirupsen/logrus"
+)
 
 func StringToInt64(s string) (int64, error) {
 	numero, err := strconv.ParseInt(s, 0, 64)
 	if err != nil {
+		log.Warn(err)
 		return 0, err
 	}
 	return numero, err
@@ -13,6 +18,7 @@ func StringToInt64(s string) (int64, error) {
 func StringToFloat64(s string) (float64, error) {
 	numero, err := strconv.ParseFloat(s, 64)
 	if err != nil {
+		log.Warn(err)
 		return 0, err
 	}
 	return numero, err
