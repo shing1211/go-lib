0d9da1a07e310ff8df9109f4d26da6ea9409bd3c
Terence Chan <shing1211@gmail.com>
2024-03-19 00:17:19 +0800

    add

---
+++ config/server.go
@@ -0,0 +1,19 @@
+package config
+
+// ServerConfig ...
+type ServerConfig struct {
+	ServerPort     string // public port of server
+	ServerMode     string
+	ServerLogLevel uint64
+	ServerJWT      struct {
+		Key    string
+		Expire int
+	}
+	ServerHashPass struct {
+		Memory      uint32
+		Iterations  uint32
+		Parallelism uint8
+		SaltLength  uint32
+		KeyLength   uint32
+	}
+}
