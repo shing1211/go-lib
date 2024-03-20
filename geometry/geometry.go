874d868ee3152aaadece49d00f2e451dd9269f9e
Terence Chan <38172860+shing1211@users.noreply.github.com>
2024-03-20 18:42:22 +0800

    update

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -18,22 +18,22 @@ type Circle struct {
 	radius float64
 }
 
-func (r rect) Area() float64 {
+func (r Rect) Area() float64 {
 	return r.width * r.height
 }
 
-func (r rect) Perim() float64 {
+func (r Rect) Perim() float64 {
 	return 2*r.width + 2*r.height
 }
 
-func (c circle) Area() float64 {
+func (c Circle) Area() float64 {
 	return math.Pi * c.radius * c.radius
 }
-func (c circle) Perim() float64 {
+func (c Circle) Perim() float64 {
 	return 2 * math.Pi * c.radius
 }
 
-func Measure(g geometry) {
+func Measure(g Geometry) {
 	fmt.Println(g)
 	fmt.Println(g.area())
 	fmt.Println(g.perim())
