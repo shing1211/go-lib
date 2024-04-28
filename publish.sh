4b38ba2286cd659f77a4ff9e68d7461013329030
Terence Chan <shing1211@gmail.com>
2024-04-28 20:38:06 +0800

    update build scipt

--- publish.sh
+++ publish.sh
@@ -5,6 +5,6 @@ rm go.sum
 go mod init github.com/shing1211/go-lib
 go mod tidy
 set GOPROXY=proxy.golang.org
-git tag v0.1.15
-git push origin v0.1.15
-go list -m github.com/shing1211/go-lib@v0.1.15
+git tag v0.1.16
+git push origin v0.1.16
+go list -m github.com/shing1211/go-lib@v0.1.16
